40f9b85b6622af42baaf763c61e90c4f27ba4401
kellenmay <kellenbmay@gmail.com>
2021-08-10 17:59:21 -0400

    Add button component and add to navbar

---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
+
+    
+
+
+    return (
+        <div>
+        <button onClick={onClick} stlye={{ backgroundColor: color }} className='btn'>
+        {text}
+        </button>
+
+        </div>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelBlue'
+
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+
+export default Button
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,18 @@
+import Button from './Button'
+
+
 const NavBar = (props) => {
+
+const onClick = () => {
+    console.log('Click')
+}
+
     return (
-        <header>
+        <header className='header'>
             <h1 style={{color: 'black', backgroundColor: 'red'}}>{props.title}</h1>
+        <br></br>
+        <Button color='blue' text='New Entry' onClick={onClick} />
+        <Button color='blue' text='Your Journal' onClick={onClick}/>
         </header>
     )
 }
