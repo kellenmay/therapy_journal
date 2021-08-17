5205e2fa0fb556413a7a965c8f40547415513320
kellenmay <kellenbmay@gmail.com>
2021-08-17 12:18:38 -0400

    remove user auth

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
-import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router>
-    <Auth0ProviderWithHistory>
+
       <App />
-    </Auth0ProviderWithHistory>
-  </Router>,
+
   </React.StrictMode>,
   document.getElementById('root')
 );
